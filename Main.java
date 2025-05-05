33fe2b8355960ee0264e50a7a6ec11e400d112c0
rosalindc2 <rosalindc2@nycstudents.net>
2025-05-05 00:27:03 +0000

    done2

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main
     {
         FrogSimulation sim = new FrogSimulation(24,5);
         System.out.println(sim.simulate());
+        FrogSimulation simTwo = new FrogSimulation(25,4);
+        System.out.println(simTwo.simulate());
+        FrogSimulation simThree = new FrogSimulation(31,3);
+        System.out.println(simThree.simulate());
+        FrogSimulation simFour = new FrogSimulation(-2,3);
+        System.out.println(simFour.simulate());
+        FrogSimulation simFive = new FrogSimulation(18,3);
+        System.out.println(simFive.simulate());
+
         System.out.println(sim.runSimulations(4));
     }
 }
